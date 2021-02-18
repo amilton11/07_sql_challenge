e9c3b86f6189f12c11efcf769208e18096c7869e
Abigail <amourpropre@icloud.com>
2021-02-17 18:27:45 -0600

    progress

--- 07_sql_challenge.sql
+++ 07_sql_challenge.sql
@@ -6,53 +6,53 @@ SELECT * FROM salaries
 SELECT * FROM titles
 
 -- List for each employee: employee number, last name, first name, sex, salary
-SELECT Employees.emp_no, Employees.last_name, Employees.first_name, Employees.sex, Salaries.salary
-FROM Employees
-JOIN Salaries on Salaries.emp_no = Employees.emp_no;
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries on salaries.emp_no = employees.emp_no;
 
 
 -- List employees hired in 1986: first name, last name, hire date
 SELECT last_name, first_name, hire_date
-FROM Employees
-WHERE hire_date LIKE '1986%';
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31' 
 
 
 -- List department managers: department number, department name, the manager's employee number, last name, first name
-SELECT Departments.dept_no, Departments.dept_name, Dept_Manager.emp_no, Employees.last_name, Employees.first_name
-FROM Employees
-INNER JOIN Dept_Manager on Dept_Manager.emp_no = Employees.emp_no
-INNER JOIN Departments on Departments.dept_no = Dept_Manager.dept_no
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM employees
+INNER JOIN dept_manager on dept_manager.emp_no = employees.emp_no
+INNER JOIN departments on departments.dept_no = dept_manager.dept_no
 
 
 -- List the department of each employee: employee number, last name, first name, department name
-SELECT Dept_Emp.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
-FROM Employees
-INNER JOIN Dept_Emp on Dept_Emp.emp_no = Employees.emp_no
-INNER JOIN Departments on Departments.dept_no = Dept_Emp.dept_no
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp on dept_emp.emp_no = employees.emp_no
+INNER JOIN departments on departments.dept_no = dept_emp.dept_no
 
 
 -- For employees whose first name is "Hercules" and last names begin with "B": first name, last name, sex
-SELECT Employees.first_name, Employees.last_name, Employees.sex
-FROM Employees
-WHERE Employees.first_name = 'Hercules' AND Employees.last_name LIKE 'B%'
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE employees.first_name = 'Hercules' AND employees.last_name LIKE 'B%'
 
 -- For employees in the sales department: employee number, last name, first name, department name
-SELECT Dept_Emp.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
-FROM Employees
-INNER JOIN Dept_Emp on Dept_Emp.emp_no = Employees.emp_no
-INNER JOIN Departments on Departments.dept_no = Dept_Emp.dept_no
-WHERE Departments.dept_name = 'Sales'
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp on dept_emp.emp_no = employees.emp_no
+INNER JOIN departments on departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales'
 
 
 -- For employees in the sales and developement departments: employee number, last name, first name, department name
-SELECT Dept_Emp.emp_no, Employees.last_name, Employees.first_name, Departments.dept_name
-FROM Employees
-INNER JOIN Dept_Emp on Dept_Emp.emp_no = Employees.emp_no
-INNER JOIN Departments on Departments.dept_no = Dept_Emp.dept_no
-WHERE Departments.dept_name = 'Sales' OR Departments.dept_name = 'Development'
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp on dept_emp.emp_no = employees.emp_no
+INNER JOIN departments on departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development'
 
 -- In descending order, list the frequency count of employee last names
 SELECT last_name, COUNT(first_name) AS emp_count
-FROM Employees
+FROM employees
 GROUP BY last_name
 ORDER BY last_name DESC;
--- emp_schema.sql
+++ emp_schema.sql
@@ -2,31 +2,31 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Departments" (
+CREATE TABLE "departments" (
     "dept_no" varchar(200)   NOT NULL,
     "dept_name" varchar(200)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
+    CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
-CREATE TABLE "Dept_Emp" (
+CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar(200)   NOT NULL,
-    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
-CREATE TABLE "Dept_Manger" (
+CREATE TABLE "dept_manager" (
     "dept_no" varchar(200)   NOT NULL,
     "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_Dept_Manger" PRIMARY KEY (
-        "dept_no"
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
      )
 );
 
-CREATE TABLE "Employees" (
+CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
     "emp_title_id" varchar(200)   NOT NULL,
     "birth_date" date   NOT NULL,
@@ -34,42 +34,42 @@ CREATE TABLE "Employees" (
     "last_name" varchar(200)   NOT NULL,
     "sex" varchar(200)   NOT NULL,
     "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
+    CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Salaries" (
+CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Titles" (
+CREATE TABLE "titles" (
     "title_id" varchar(200)   NOT NULL,
     "title" varchar(200)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
+    CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
-ALTER TABLE "Departments" ADD CONSTRAINT "fk_Departments_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Dept_Emp" ("dept_no");
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_emp" ("dept_no");
 
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_Manger" ADD CONSTRAINT "fk_Dept_Manger_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_Manger" ADD CONSTRAINT "fk_Dept_Manger_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Salaries" ("emp_no");
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "salaries" ("emp_no");
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "Titles" ("title_id");
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
 
